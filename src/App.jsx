0d237667d7e31308ae478045252a59ef44113ca2
connor <connorjbaba@gmail.com>
2022-09-19 18:14:50 -0400

    trying to fix google auth

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function SignIn() {
 
   function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithRedirect(provider);
+    auth.signInWithPopup(provider);
   }
 
   function guestSignIn() {
